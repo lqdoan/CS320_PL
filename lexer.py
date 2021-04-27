fe922314c05091a6eb8de9a98e9b2325ca453f71
lqdoan <48527422+lqdoan@users.noreply.github.com>
2021-04-28 01:47:25 +0700

    Add files via upload

--- lexer.py
+++ lexer.py
@@ -3,11 +3,27 @@ from readfile import *
 keywords = [
     "nil", "true", "false", "self", "super", "thisContext"
 ]
+operators = [
+    "+", "-", "*", "/", "//", "\\"
+]
+logicalOperators = []
+bitWiseOperators = []
+comparisonOperators = []
 
 def processToken(tokens: list):
     for token in tokens:
         if token.name in keywords:
             token.type = "KEYWORD"
+        elif token.name in operators:
+            token.type = "OPERATOR"
+        elif token.name in logicalOperators:
+            token.type = "LOGICAL OPERATOR"
+        elif token.name in bitWiseOperators:
+            token.type = "BIT-WISE OPERATOR"
+        elif token.name in comparisonOperators:
+            token.type = "COMPARISON OPERATOR"
+        else:
+            exit(0)
 '''
 tmp = readFile("a.txt")
 for t in tmp:
